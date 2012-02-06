65d501f714025c07f8c840d76a886f0838103f92
Shinobu Kawano / 川野忍 <kawanoshinobu@gmail.com>
2012-02-06 17:44:50 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+Sencha Touch 2 Recipes
+=======
+
+This is a tiny collection of sencha touch 2 recipes.
+http://dev.ariel-networks.com/
+
+-----
+
+- Recipes:1 To emulate the Back button. 
+  sample: http://recipe1.senchafy.com/
+
+
+license
+-------
+
+Copyright (c) 2012 Shinobu Kawano. This software is licensed under the MIT License.
+
---
+++ recipe1/app.js
@@ -0,0 +1,53 @@
+Ext.define('recipe.controller.SampleController', {
+  extend: 'Ext.app.Controller',
+  config: {
+    refs: {
+      tabPanel : 'tabpanel'
+    },
+    control: {
+      tabPanel: {
+        activeitemchange: 'onActiveItemChange'
+      }
+    },
+    routes: {
+      // Regular expression "page/([% a-zA-Z0-9-\_\s,]+)" will be generated.
+      'page/:season': 'onRoute',     
+    }
+  },
+
+  onRoute: function (season) {
+    var tabPanel = this.getTabPanel(),
+    target = tabPanel.down('container[title=' + season + ']');
+    tabPanel.setActiveItem(target);
+  },
+
+  onActiveItemChange: function (self, newItem) {
+    window.location.hash = 'page/' + newItem.config.title;
+  }
+});
+
+Ext.application({
+  name: 'recipe',
+  controllers: [
+    'SampleController'
+  ],
+  launch: function () {
+    Ext.create('Ext.TabPanel', {
+      fullscreen: true,
+      items: [
+        {
+          title: 'spring'
+        },
+        {
+          title: 'summer'
+        },
+        {
+          title: 'fall'
+        },
+        {
+          title: 'winter'
+        }
+      ]
+    });
+  }
+});
